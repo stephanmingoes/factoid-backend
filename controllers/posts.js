7ed622a18364be9f4e742ae4cd8858767b4cc3f4
Stephan Mingoes <stephsvpreme@gmail.com>
2021-07-10 06:25:06 -0500

    new features

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,10 +3,48 @@ import express from "express";
 import mongoose from "mongoose";
 const router = express.Router();
 
-export const getPosts = async (req, res) => {
+export const getPostBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
   try {
-    const postMessages = await PostMessage.find();
-    res.status(200).json(postMessages);
+    const title = new RegExp(searchQuery, "i");
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    res.send(404).json({ message: err.message });
+  }
+};
+export const getPosts = async (req, res) => {
+  const { page } = req.query;
+  try {
+    const Limit = 8;
+    const startIndex = (Number(page) - 1) * 8; //get starting index of every page
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(Limit)
+      .skip(startIndex);
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / Limit),
+    });
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
--- routes/posts.js
+++ routes/posts.js
@@ -5,11 +5,15 @@ import {
   updatePost,
   deletePost,
   likePost,
+  getPostBySearch,
+  getPost,
 } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 const router = express.Router();
 
+router.get("/search?", getPostBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
